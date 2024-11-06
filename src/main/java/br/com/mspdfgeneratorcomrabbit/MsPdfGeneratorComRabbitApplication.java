0cd9e00edad06ec398d42f6e162b83abf599f9fa
Israel Barberino <israel.barberino@compasso.com.br>
2024-11-06 13:46:36 -0300

    feat: Novo ms-pdf-generator

---
+++ src/main/java/br/com/mspdfgeneratorcomrabbit/MsPdfGeneratorComRabbitApplication.java
@@ -0,0 +1,13 @@
+package br.com.mspdfgeneratorcomrabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPdfGeneratorComRabbitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPdfGeneratorComRabbitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mspdfgeneratorcomrabbit/MsPdfGeneratorComRabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mspdfgeneratorcomrabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPdfGeneratorComRabbitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
